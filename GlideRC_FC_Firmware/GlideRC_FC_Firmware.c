0738f20f0c0ec8875340484a633026b5d44577f4
Person-0 <atulyajain28122007@gmail.com>
2026-06-01 23:44:15 +0530

    better "turn-off" approach

--- GlideRC_FC_Firmware/GlideRC_FC_Firmware.c
+++ GlideRC_FC_Firmware/GlideRC_FC_Firmware.c
@@ -7,6 +7,7 @@
     #define sleep_ms(ms) usleep((ms) * 1000)
 #endif
 
+#include "src/config.h"
 #include "src/hardware.h"
 
 void sleep_forever() {
@@ -14,8 +15,13 @@ void sleep_forever() {
     while (1) sleep_ms(1000);
 }
 
+void disable_components() {
+    disable_motor();
+    disable_control_servos();
+}
+
 // State variables
-bool imu_err = false;
+bool EXCEPTION_OCCURED = false;
 float imu_data[6] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 
 // Recieves data from the receiver
@@ -47,16 +53,17 @@ int main() {
     // IMU
     if (setup_imu() > 0) {
         printf("FATAL IMU error, Disabling motor and servos.\n");
-        imu_err = true;
-        disable_motor();
-        disable_control_servos();
-        sleep_forever();
+        EXCEPTION_OCCURED = true;
     }
 
     // main loop
-    while (1) {
+    while (!EXCEPTION_OCCURED) {
         read_imu(imu_data);
         printf("imu data read\n");
         sleep_ms(100);
     }
+
+    printf("EXCEPTION_OCCURED, main loop exited\n");
+    disable_components();
+    sleep_forever();
 }
